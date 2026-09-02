63d6c778b69ccc6834089370a0b59d29616e6b3a
samakshkambxj <samakshk4@gmail.com>
2026-09-03 03:36:41 +0530

    Galaxian: Welcome Voltage
    Signed-off-by: samakshkambxj <samakshk4@gmail.com>

--- voltage_Galaxian.mk
+++ voltage_Galaxian.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/nothing/Galaxian/device.mk)
 
 # Inherit some common lineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_Galaxian
+PRODUCT_NAME := voltage_Galaxian
 PRODUCT_DEVICE := Galaxian
 PRODUCT_MANUFACTURER := Nothing
 PRODUCT_BRAND := Nothing
