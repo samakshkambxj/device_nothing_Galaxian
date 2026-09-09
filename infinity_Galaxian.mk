9a08fb0c51b02b24e3bf74cec5d6024d3df43ca3
Samaksh Kamboj <samakshk4@gmail.com>
2026-09-09 20:16:24 +0000

    Galaxian: Welcome Infinity
    Signed-off-by: Samaksh Kamboj <samakshk4@gmail.com>

--- infinity_Galaxian.mk
+++ infinity_Galaxian.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/nothing/Galaxian/device.mk)
 
 # Inherit some common lineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_Galaxian
+PRODUCT_NAME := infinity_Galaxian
 PRODUCT_DEVICE := Galaxian
 PRODUCT_MANUFACTURER := Nothing
 PRODUCT_BRAND := Nothing
@@ -28,3 +28,16 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=Galaxian \
     BuildDesc="sys_mssi_64_64only_ww_armv82-user 15 AP3A.240905.015.A2 2607021815 release-keys" \
     BuildFingerprint=Nothing/Galaxian/Galaxian:16/BP2A.250605.031.A3/2607021815:user/release-keys
+
+# Maintainer Name
+INFINITY_MAINTAINER := "Samakshhhh"
+
+# Whether the device supports Fingerprint On Display
+TARGET_HAS_UDFPS := true
+
+# Whether Including Google Apps
+WITH_GAPPS := true
+
+ro.product.marketname=Nothing Phone (3a) Lite
+ro.infinity.soc=Mediatek Dimensity 7300 Pro
+ro.infinity.camera=50MP + 8MP + 8MP + 2MP
