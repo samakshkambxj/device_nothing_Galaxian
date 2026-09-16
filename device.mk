47cacfab32c6ca731ca98ad2c76ce1197271fe00
samakshkambxj <samakshk4@gmail.com>
2026-09-16 20:47:39 +0530

    Galaxian: Route spatial output to BT A2DP
    Add spatializer support in the BT HAL module instead of
    declaring BT A2DP devices inside primary (which breaks all
    BT audio on MTK - APM opens A2DP via the wrong HAL).
    
    - Local bluetooth_audio_policy_configuration.xml based on
      frameworks/av 7_0 with spatial output mixPort
      (PCM_16_BIT/48000/stereo, AUDIO_OUTPUT_FLAG_SPATIALIZER)
    - Route a2dp output + spatial output to BT A2DP
      Out/Headphones/Speaker, keeping a2dp output so
      non-spatial BT keeps working
    - device.mk uses the local BT policy instead of stock
    
    Signed-off-by: samakshkambxj <samakshk4@gmail.com>

--- device.mk
+++ device.mk
@@ -92,7 +92,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc) \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 
 PRODUCT_PACKAGES += \
